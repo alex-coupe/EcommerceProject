e05d1f8f4f449d07375c16f71fa1e8fb63bcd0b7
Alex <acooupe@gmail.com>
2021-02-07 12:38:18 +0000

    More work on dockerizing

--- source/Api/Services/ProductCatalogService/src/DatabaseContext.ts
+++ source/Api/Services/ProductCatalogService/src/DatabaseContext.ts
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
 
 mongoose.set('useCreateIndex', true);
+const url:string = 'mongodb://localhost:27017/';
+const user:string = 'root';
+const password:string = 'root';
 
-try {
-    mongoose.connect('mongodb+srv://admin:admin@cluster0.al1l0.mongodb.net/test?retryWrites=true&w=majority',  { useNewUrlParser: true, useUnifiedTopology: true });
-    }
-    catch (error)
-    {
-        console.log(error);
-    }
+mongoose.Promise = global.Promise;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    user: user,
+    pass: password
+}).then(() => {
+    console.log('successfully connected to the database');
+}).catch(err => {
+    console.log('error connecting to the database');
+    process.exit();
+});
     
     export = mongoose.connection;

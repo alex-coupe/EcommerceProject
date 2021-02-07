20838659faa132625f8c50ec278af60ab7442dd4
Alex <acooupe@gmail.com>
2021-02-07 18:32:49 +0000

    Finally connecting to our gateway which is then speaking to products service

---
+++ source/Api/Gateway/Controllers/ProductServicesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductServicesController : ControllerBase
+    {
+        private IHttpClientFactory _httpClientFactory;
+        public ProductServicesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> TestProductsLink()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "http://products_api:5001/api/v1/products");
+
+            var _client = _httpClientFactory.CreateClient();
+
+            var response = await _client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok("Successfully Spoke To Products Service");
+            }
+
+            return NotFound("Couldn't reach products service :(");
+
+        }
+    }
+}
--- source/Api/Gateway/Startup.cs
+++ source/Api/Gateway/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APIGateway
+namespace Gateway
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace APIGateway
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIGateway", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gateway", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace APIGateway
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIGateway v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway v1"));
             }
 
             app.UseHttpsRedirection();
